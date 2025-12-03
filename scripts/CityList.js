0b01b42b24bbc7c2f6e6a8c7aba1bf599d0998d9
Evan Alford <evan.al4d@gmail.com>
2025-12-03 15:13:00 -0600

    completed rendering to main need css

--- scripts/CityList.js
+++ scripts/CityList.js
@@ -1,13 +1,29 @@
-import { getWalkers } from "./database.js"
+import { getWalkers,getCities } from "./database.js"
 
 const walkers = getWalkers()
+const cities = getCities()
 
+document.addEventListener("click",(clickEvt) => {
+        const cityTarget = clickEvt.target
+
+            if (cityTarget.dataset.type === "city" ){
+                const cityId = parseInt(cityTarget.dataset.cityid)
+
+                //Find all walkers for the city
+                const walkersInCity = walkers.filter(walker => walker.cityId === cityId)
+
+                //Create list of walker names
+                const walkerNames = walkersInCity.map(walker => walker.name).join(", ")
+
+                window.alert(`${walkerNames} is servicing this city`)
+            }
+        })
 
 export const CityList = () => {
     let citiesHTML = "<ol>"
 
-    for (const walker of walkers) {
-        citiesHTML += `<li>${walker.city}</li>`
+    for (const city of cities) {
+        citiesHTML += `<li data-type="city" data-cityid ="${city.id}">${city.city}</li>`
     }
 
     citiesHTML += "</ol>"
